da5fd42ed00fd8c189d80a773a6dbcc401c73bbf
jes <suni980215@gmail.com>
2024-08-08 21:03:34 +0900

    feat: 빌보드 삭제 로직 연결

--- app/(dashboard)/[storeId]/(routes)/billboards/_components/cell-action.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/_components/cell-action.tsx
@@ -14,24 +14,39 @@ import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
 import { Copy, Edit, MoreHorizontal, Trash } from "lucide-react";
 import { toast } from "sonner";
+import axios from "axios";
+import { useParams, useRouter } from "next/navigation";
 
 type Props = {
   data: BillboardColumns;
 };
 
 export const CellAction: React.FC<Props> = ({ data }) => {
+  const router = useRouter();
+  const params = useParams();
   const [isOpen, setIsOpen] = useState(false);
-
   const [isDeletePending, startDeleteTransition] = useTransition();
 
-  //빌보드 수정
+  // 빌보드 수정 클릭 시 수정 페이지로 이동
   const onUpdateClick = () => {
-    //수정 api
+    router.push(`/${params.storeId}/billboards/${data.id}`);
   };
 
-  //빌보드 삭제
+  // 빌보드 삭제
   const onDeleteConfirm = () => {
-    //삭제 api
+    startDeleteTransition(async () => {
+      try {
+        await axios.delete(`/api/${params.storeId}/billboards/${data.id}`);
+        router.refresh();
+        toast.success("삭제되었습니다.", { id: "delete-billboard" });
+      } catch (error) {
+        toast.error("모든 물품과 카테고리를 삭제했는지 확인하세요.", {
+          id: "delete-billboard",
+        });
+      } finally {
+        setIsOpen(false);
+      }
+    });
   };
 
   //클립보드에 클릭한 빌보드 아이디 복사
