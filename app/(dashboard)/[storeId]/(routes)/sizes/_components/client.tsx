a32bd9df70302168074c4481a25f6f9979f250e2
jes <suni980215@gmail.com>
2024-08-26 18:19:08 +0900

    F:size 리스트 조회 컴포넌트 생성

---
+++ app/(dashboard)/[storeId]/(routes)/sizes/_components/client.tsx
@@ -0,0 +1,7 @@
+export const SizeClient = () => {
+  return (
+    <>
+      <div>size client components</div>
+    </>
+  );
+};
--- app/(dashboard)/[storeId]/(routes)/sizes/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/sizes/page.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { SizeClient } from "@/app/(dashboard)/[storeId]/(routes)/sizes/_components/client";
 
 type Props = {
   params: {
@@ -9,7 +10,9 @@ type Props = {
 const SizesPage: React.FC<Props> = async ({ params }) => {
   return (
     <div className="flex-col">
-      <div className="flex-1 space-y-4 p-8 pt-6">{/*form 자리*/}</div>
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <SizeClient />
+      </div>
     </div>
   );
 };
