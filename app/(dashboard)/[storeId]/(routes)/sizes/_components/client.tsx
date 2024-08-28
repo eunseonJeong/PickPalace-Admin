7fa1f2adb084ea402d4879431bf1d04a8b5a7369
jes <suni980215@gmail.com>
2024-08-28 18:42:57 +0900

    F:size 리스트 조회 코드 및  테이블에 들어갈 컬럼 추가

--- app/(dashboard)/[storeId]/(routes)/sizes/_components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/sizes/_components/client.tsx
@@ -1,6 +1,12 @@
 //사이즈 리스트 조회
 
-export const SizeClient = () => {
+import { SizeColumn } from "@/app/(dashboard)/[storeId]/(routes)/sizes/_components/columns";
+
+type Props = {
+  data: SizeColumn[];
+};
+
+export const SizeClient: React.FC<Props> = () => {
   return (
     <>
       <div>size client components</div>
--- app/(dashboard)/[storeId]/(routes)/sizes/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/sizes/page.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { SizeClient } from "@/app/(dashboard)/[storeId]/(routes)/sizes/_components/client";
+import db from "@/lib/prismadb";
+import { format } from "date-fns";
+import { ko } from "date-fns/locale";
 
 type Props = {
   params: {
@@ -7,13 +10,33 @@ type Props = {
   };
 };
 
-const SizesPage: React.FC<Props> = async ({ params }) => {
+const SizesPage: React.FC<Props> = async ({
+  params,
+}: {
+  params: { storeId: string };
+}) => {
+  const sizesList = await db.size.findMany({
+    where: {
+      storeId: params.storeId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  const formattedSizes = sizesList.map((item) => ({
+    id: item.id,
+    name: item.name,
+    value: item.value,
+    createdAt: format(item.createdAt, "yyyy-MM-dd", { locale: ko }),
+  }));
+
   return (
-    <div className="flex-col">
-      <div className="flex-1 space-y-4 p-8 pt-6">
-        <SizeClient />
+    <>
+      <div className="space-y-4 p-8 pt-6">
+        <SizeClient data={formattedSizes} />
       </div>
-    </div>
+    </>
   );
 };
 
