9edaed089488dfc67b648eb5244250c343081c42
jes <suni980215@gmail.com>
2024-08-29 18:54:04 +0900

    F:size 리스트 조회 코드 수정

--- app/(dashboard)/[storeId]/(routes)/sizes/_components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/sizes/_components/client.tsx
@@ -1,15 +1,44 @@
 //사이즈 리스트 조회
+"use client";
 
-import { SizeColumn } from "@/app/(dashboard)/[storeId]/(routes)/sizes/_components/columns";
+import React from "react";
+import {
+  columns,
+  SizeColumn,
+} from "@/app/(dashboard)/[storeId]/(routes)/sizes/_components/columns";
+import { useParams, useRouter } from "next/navigation";
+import { Heading } from "@/components/ui/heading";
+import { Button } from "@/components/ui/button";
+import { Plus } from "lucide-react";
+import { Separator } from "@/components/ui/separator";
+import { DataTable } from "@/components/ui/data-table";
+import { ApiList } from "@/components/ui/api-list";
 
 type Props = {
   data: SizeColumn[];
 };
 
-export const SizeClient: React.FC<Props> = () => {
+export const SizeClient: React.FC<Props> = ({ data }) => {
+  const router = useRouter();
+  const params = useParams();
+
   return (
     <>
-      <div>size client components</div>
+      <div className="flex items-center justify-between">
+        <Heading
+          title={`Sizes (${data.length})`}
+          description="사이즈를 관리할 수 있습니다."
+        />
+        <Button onClick={() => router.push(`/${params.storeId}/sizes/new`)}>
+          <Plus className="mr-2 size-4" />
+          사이즈 생성
+        </Button>
+      </div>
+      <Separator />
+      <DataTable searchKey="name" columns={columns} data={data} />
+      <Heading title="API" description="사이즈를 위한 API를 제공합니다." />
+      <Separator />
+      <ApiList entityName="sizes" entityIdName="sizeId" />
     </>
   );
 };
