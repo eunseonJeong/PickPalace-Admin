4a9315dcfa5e619f9eb84f6db961d823d6868df3
jes <suni980215@gmail.com>
2024-08-31 17:21:58 +0900

    F:size 리스트 상세 조회 코드 수정

--- app/(dashboard)/[storeId]/(routes)/sizes/[sizeId]/_components/size-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/sizes/[sizeId]/_components/size-form.tsx
@@ -1,6 +1,12 @@
 //사이즈 등록 & 수정 컴포넌트
 
-export const SizeForm = () => {
+import { Size } from "@prisma/client";
+
+type Props = {
+  initialData: Size | null;
+};
+
+export const SizeForm: React.FC<Props> = ({ initialData }) => {
   return (
     <>
       <div>size form</div>
