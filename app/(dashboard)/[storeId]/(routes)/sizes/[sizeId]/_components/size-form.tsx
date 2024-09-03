16ae3573c879bfeabeff260b8d6ecadcea5cc8a2
jes <suni980215@gmail.com>
2024-09-03 19:46:36 +0900

    F: size 등록 폼 컴포넌트 추가

--- app/(dashboard)/[storeId]/(routes)/sizes/[sizeId]/_components/size-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/sizes/[sizeId]/_components/size-form.tsx
@@ -9,7 +9,7 @@ import { Size } from "@prisma/client";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useParams, useRouter } from "next/navigation";
 import { useForm } from "react-hook-form";
-import { Trash } from "lucide-react";
+import { Loader2, Trash } from "lucide-react";
 
 import { Separator } from "@/components/ui/separator";
 import { Button } from "@/components/ui/button";
@@ -23,14 +23,6 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
-import { AlertModal } from "@/components/modal/alert-modal";
 
 const formSchema = z.object({
   name: z.string().min(1),
@@ -42,41 +34,90 @@ type Props = {
 };
 
 export const SizeForm: React.FC<Props> = ({ initialData }) => {
+  const params = useParams();
+  const router = useRouter();
+
+  const [isPending, startTransition] = useTransition();
+
+  const title = initialData ? "사이즈 수정" : "사이즈 생성";
+
+  const description = initialData
+    ? "사이즈를 수정합니다."
+    : "사이즈를 생성합니다.";
+
+  const action = initialData ? "수정하기" : "생성하기";
+
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: initialData || { name: "", value: "" },
   });
 
+  // 사이즈 생성
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    startTransition(async () => {
+      try {
+        await axios.post(`/api/${params.storeId}/sizes`, values);
+        toast.success("사이즈가 생성되었습니다.", {
+          id: "category",
+        });
+        router.push(`/${params.storeId}/sizes`);
+        router.refresh();
+      } catch (e) {
+        toast.error("생성에 실패했습니다.");
+      }
+    });
+  };
+
   return (
     <>
+      <div className="flex items-center justify-between">
+        <Heading title={title} description={description} />
+      </div>
+      <Separator />
       <Form {...form}>
-        <form>
-          <FormField
-            control={form.control}
-            name="name"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>사이즈 명칭</FormLabel>
-                <FormControl>
-                  <Input placeholder="사이즈 명칭을 입력하세요." {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="value"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>사이즈 수치</FormLabel>
-                <FormControl>
-                  <Input placeholder="수치를 입력하세요." {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="w-full space-y-8"
+        >
+          <div className="grid grid-cols-3 gap-8">
+            <FormField
+              name="name"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>사이즈 명칭</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={isPending}
+                      placeholder="사이즈 명칭을 입력하세요."
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              name="value"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>수치</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={isPending}
+                      placeholder="수치를 입력하세요."
+                      {...field}
+                    />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+          </div>
+          <Button type="submit" className="ml-auto" disabled={isPending}>
+            {isPending && <Loader2 className="mr-2 size-4 animate-spin" />}
+            {action}
+          </Button>
         </form>
       </Form>
     </>
