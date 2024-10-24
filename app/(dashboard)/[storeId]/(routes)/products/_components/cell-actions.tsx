fe0a00bb0def2eaba226101a4fa4d7c462b2be22
jes <suni980215@gmail.com>
2024-10-24 14:12:09 +0900

    F: product 클립보드로 복사 추가

--- app/(dashboard)/[storeId]/(routes)/products/_components/cell-actions.tsx
+++ app/(dashboard)/[storeId]/(routes)/products/_components/cell-actions.tsx
@@ -9,9 +9,10 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { Separator } from "@/components/ui/separator";
-import { Edit, MoreHorizontal } from "lucide-react";
+import { Copy, Edit, MoreHorizontal } from "lucide-react";
 import { useParams, useRouter } from "next/navigation";
 import { ProductColumn } from "./columns";
+import { toast } from "sonner";
 
 type Props = {
   data: ProductColumn;
@@ -23,6 +24,13 @@ export const CellAction = ({ data }: Props) => {
 
   const onEditClick = () => {
     router.push(`/${params.storeId}/products/${data.id}`);
+  };
+  // 아이디를 클립보드로 복사
+  const onCopyClick = () => {
+    navigator.clipboard.writeText(data.id);
+    toast.success("클립보드에 복사되었습니다.", {
+      id: "copy-to-clipboard",
+    });
   };
 
   return (
@@ -37,6 +45,10 @@ export const CellAction = ({ data }: Props) => {
         <DropdownMenuContent align="end">
           <DropdownMenuLabel>Actions</DropdownMenuLabel>
           <Separator />
+          <DropdownMenuItem onClick={onCopyClick}>
+            <Copy className="mr-2 size-4" />
+            아이디 복사
+          </DropdownMenuItem>
           <DropdownMenuItem onClick={onEditClick}>
             <Edit className="mr-2 size-4" />
             상품 수정
